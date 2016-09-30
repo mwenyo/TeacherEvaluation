a446b3acbdc809b8667183d827473eb17a190bf1
mwenyo <mwenyo@gmail.com>
2016-09-30 09:42:08 -0300

    questionario_respondido

--- TeacherEvaluation/questionarios/models.py
+++ TeacherEvaluation/questionarios/models.py
@@ -8,4 +8,10 @@ class Questionario(models.Model):
 	
 	administrador = models.ForeignKey(Administrador)
 	data = models.DateTimeField()
-	pergunta = models.ManyToManyField(Pergunta)
+	pergunta = models.ManyToManyField(Pergunta, through='Questionario_Pergunta')
+	
+class Questionario_Pergunta(models.Model):
+	questionario = models.ForeignKey(Questionario, on_delete=models.CASCADE)
+	pergunta = models.ForeignKey(Pergunta, on_delete=models.CASCADE)
+	data_resposta = models.DateTimeField()
+	resposta = models.DecimalField(max_digits=3, decimal_places=1)
